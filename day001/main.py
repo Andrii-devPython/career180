8f7ba3c3e42290f38f576fff581aeb644dd4f566
Andrii-devPython <aruabush@gmail.com>
2026-08-12 12:34:16 +0200

    Update main.py

--- day001/main.py
+++ day001/main.py
@@ -1,1 +1,15 @@
-print("Привіт")
+# name = input("Як тебе звати? ")
+# age = int(input("Скільки тобі років? "))
+# country = input("Де ти живеш ")
+
+# print(f"Привіт, {name}")
+# print(f"Тобі аж {age}")
+# print(f"Ти живеш у {country}")
+
+worker_name = input("Put your name: ")
+hourly_rate = float(input("Put your salary per hour: "))
+hours_worker = int(input("Put your working hours: "))
+salary = hourly_rate * hours_worker
+
+print(f"Привіт , {worker_name}! ")
+print(f"Твоя зарплата: {salary} PLN. ")
